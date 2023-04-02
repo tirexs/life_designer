e79293435056b48a4315509298b7bec5040bc11a
леонид сарбукоа <sarbukov.leonid@gmail.com>
2023-04-02 19:49:12 +0300

    работает добавление/удаление категорий

--- life_designer/obj/Debug/net6.0-windows/page/Page1.g.i.cs
+++ life_designer/obj/Debug/net6.0-windows/page/Page1.g.i.cs
@@ -47,13 +47,13 @@ namespace life_designer.page {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/life_designer;component/page/page1.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/life_designer;V1.0.0.0;component/page/page1.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\page\Page1.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -63,7 +63,7 @@ namespace life_designer.page {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
