075a94fc82377e6f4d62498bf790520c1a21d5e1
леонид сарбукоа <sarbukov.leonid@gmail.com>
2023-04-04 20:54:58 +0300

    разбираемся с основной частью приложения

--- life_designer/MainWindow.xaml.cs
+++ life_designer/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Windows;
-using life_designer.page;
 namespace life_designer
 {
     /// <summary>
@@ -12,27 +11,13 @@ namespace life_designer
         public MainWindow()
         {
             InitializeComponent();
-            MainFrame.Content = new MainPage();
+            
 
         }
 
         
 
-        private void Button_Cprivet_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Navigate( new page.Page1());
-           
-        }
-
-        private void Button_Chome_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Navigate(new page.Page2());
-        }
-
-        private void Button_Cwork_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Navigate(new page.Page3());
-        }
+        
        
         private void add_category_Click(object sender, RoutedEventArgs e)
         {
